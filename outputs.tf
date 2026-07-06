8788e84ef99c4d92fd75d33d1ffe0ae02a02d208
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,43 @@
 output "system_center_virtual_machine_manager_servers" {
   description = "All system_center_virtual_machine_manager_server resources"
   value       = azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers
+  sensitive   = true
+}
+output "system_center_virtual_machine_manager_servers_custom_location_id" {
+  description = "List of custom_location_id values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.custom_location_id]
+}
+output "system_center_virtual_machine_manager_servers_fqdn" {
+  description = "List of fqdn values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.fqdn]
+}
+output "system_center_virtual_machine_manager_servers_location" {
+  description = "List of location values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.location]
+}
+output "system_center_virtual_machine_manager_servers_name" {
+  description = "List of name values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.name]
+}
+output "system_center_virtual_machine_manager_servers_password" {
+  description = "List of password values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.password]
+  sensitive   = true
+}
+output "system_center_virtual_machine_manager_servers_port" {
+  description = "List of port values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.port]
+}
+output "system_center_virtual_machine_manager_servers_resource_group_name" {
+  description = "List of resource_group_name values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.resource_group_name]
+}
+output "system_center_virtual_machine_manager_servers_tags" {
+  description = "List of tags values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.tags]
+}
+output "system_center_virtual_machine_manager_servers_username" {
+  description = "List of username values across all system_center_virtual_machine_manager_servers"
+  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.username]
 }
 
