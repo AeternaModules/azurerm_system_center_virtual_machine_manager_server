384b49363b8796a416cd431dfef7df6e418e6bc4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:41 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.system_center_virtual_machine_manager_servers : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_system_center_virtual_machine_manager_server" "system_center_virtual_machine_manager_servers" {
   for_each = var.system_center_virtual_machine_manager_servers
 
@@ -5,7 +10,7 @@ resource "azurerm_system_center_virtual_machine_manager_server" "system_center_v
   fqdn                = each.value.fqdn
   location            = each.value.location
   name                = each.value.name
-  password            = each.value.password
+  password            = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   resource_group_name = each.value.resource_group_name
   username            = each.value.username
   port                = each.value.port
