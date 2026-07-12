8e9ab5b453894ac8620e754df626fc8f939e2570
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "system_center_virtual_machine_manager_servers" {
-  description = "All system_center_virtual_machine_manager_server resources"
-  value       = azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers
-  sensitive   = true
-}
 output "system_center_virtual_machine_manager_servers_custom_location_id" {
-  description = "List of custom_location_id values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.custom_location_id]
+  description = "Map of custom_location_id values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.custom_location_id }
 }
 output "system_center_virtual_machine_manager_servers_fqdn" {
-  description = "List of fqdn values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.fqdn]
+  description = "Map of fqdn values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.fqdn }
 }
 output "system_center_virtual_machine_manager_servers_location" {
-  description = "List of location values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.location]
+  description = "Map of location values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.location }
 }
 output "system_center_virtual_machine_manager_servers_name" {
-  description = "List of name values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.name]
+  description = "Map of name values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.name }
 }
 output "system_center_virtual_machine_manager_servers_password" {
-  description = "List of password values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.password]
+  description = "Map of password values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.password }
   sensitive   = true
 }
 output "system_center_virtual_machine_manager_servers_port" {
-  description = "List of port values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.port]
+  description = "Map of port values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.port }
 }
 output "system_center_virtual_machine_manager_servers_resource_group_name" {
-  description = "List of resource_group_name values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.resource_group_name]
+  description = "Map of resource_group_name values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.resource_group_name }
 }
 output "system_center_virtual_machine_manager_servers_tags" {
-  description = "List of tags values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.tags]
+  description = "Map of tags values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.tags }
 }
 output "system_center_virtual_machine_manager_servers_username" {
-  description = "List of username values across all system_center_virtual_machine_manager_servers"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : v.username]
+  description = "Map of username values across all system_center_virtual_machine_manager_servers, keyed the same as var.system_center_virtual_machine_manager_servers"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_server.system_center_virtual_machine_manager_servers : k => v.username }
 }
 
